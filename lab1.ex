685c8f24e3bec00c13f8e0e7cfd0e46dbbc2454d
HoshiRumi <dimabelyh86@gmail.com>
2026-02-18 04:57:26 +0300

    first commit

---
+++ lab1.ex
@@ -0,0 +1,36 @@
+n = IO.gets("Введите число: ") |> String.trim() |> String.to_integer()
+
+defmodule Cipocka do
+  def generate(n) do
+    if n < 2 do
+      IO.puts(
+        "Слова не может содержать меньше двух символов, так-как что бы попасть в x нам нужно обязательно b и с короче минимальное слово bc. Дурак >/////< "
+      )
+    end
+
+    if n == 2 do
+      IO.puts("bc")
+    end
+
+    if n >= 2 do
+      a = n - 2
+      b = Enum.random(0..a)
+      c = a - b
+      IO.inspect(a: a, b: b, c: c, n: n)
+
+      for _ <- 0..(b - 1) do
+        IO.write(Enum.random(["a", "b"]))
+      end
+
+      IO.write("b")
+
+      for _ <- 0..(c - 1) do
+        IO.write(Enum.random(["c", "d"]))
+      end
+
+      IO.write("c")
+    end
+  end
+end
+
+Cipocka.generate(n)
